67822789aa5687d04e1b07b4db306786b48c5ff5
sandipsaha597 <40121135+sandipsaha597@users.noreply.github.com>
2019-05-17 15:43:40 +0530

    Add files via upload

---
+++ precache-manifest.8aab3ad1f5703219c917b73048ea353d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e784a97d0f39d9eff96dcb829deff8",
+    "url": "/Sample-text-generator-React-app/index.html"
+  },
+  {
+    "revision": "f3d0824a209c33b5a567",
+    "url": "/Sample-text-generator-React-app/static/css/main.eff4c089.chunk.css"
+  },
+  {
+    "revision": "979821f6756ba7dc378a",
+    "url": "/Sample-text-generator-React-app/static/js/2.7569e5d4.chunk.js"
+  },
+  {
+    "revision": "f3d0824a209c33b5a567",
+    "url": "/Sample-text-generator-React-app/static/js/main.8aa15f66.chunk.js"
+  },
+  {
+    "revision": "4dc8e6f5f668e2a17cd0",
+    "url": "/Sample-text-generator-React-app/static/js/runtime~main.0ba9c27e.js"
+  }
+]);
